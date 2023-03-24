e322adacb74eac136f9ecde4bbb4b0c1e97e271b
927joanna <sot.joanna@gmail.com>
2023-03-25 02:40:55 +0800

    added variables to js

---
+++ script.js
@@ -0,0 +1,29 @@
+// variables
+
+const userInputArray = []; //stores user inputs - reset every level
+const answerArray = []; //stores computer generated answer - add to it every level, reset every game over
+
+var userInputIndex = -1; //keeps track of user's number of inputs - add to it every button press, reset every level
+var gamePage = "start"; // or playing or end 
+
+
+
+const gameGridEl = document.querySelector(".game-grid");
+
+const levelNumberSpan = document.querySelector("#level");
+
+
+// functions
+function addNewNumberToAnswer() {
+  answerArray.push(Math.floor(Math.random()*9 + 1));
+}
+
+function addNewNumberToInputArray(number) {
+  userInputArray.push(number);
+}
+
+
+// DOM Manipulation
+
+levelNumberSpan.innerText = answerArray.length;
+
