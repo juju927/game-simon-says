f059d1568233613538ff8ca61f6839463b2a598c
927joanna <sot.joanna@gmail.com>
2023-03-27 15:34:49 +0800

    start button clicked event is gOODE
    getting some error with button becoming null...?

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // variables
-
 const userInputArray = []; //stores user inputs - reset every level
 const answerArray = []; //stores computer generated answer - add to it every level, reset every game over
 
 var userInputIndex = -1; //keeps track of user's number of inputs - add to it every button press, reset every level
-var gamePage = "start"; // or playing or end 
+var gamePage = "start"; // or playing or end
 
 var flashInterval = 1;
 var boardActive = false;
@@ -12,41 +11,69 @@ var boardActive = false;
 const gameGridEl = document.querySelector(".game-grid");
 const startButtonEl = document.querySelector("#start-button");
 
-const startGameOverlay = document.querySelector('.overlay-start');
-const endGameOverlay = document.querySelector('.overlay-end');
+const startGameOverlay = document.querySelector(".overlay-start");
+const endGameOverlay = document.querySelector(".overlay-end");
 
 const levelNumberSpan = document.querySelector("#level");
 
+const backgroundEl = document.querySelector("body");
 
 // functions
 function addNewNumberToAnswer() {
-  answerArray.push(String(Math.floor(Math.random()*9 + 1)));
+  answerArray.push(Math.floor(Math.random()*9 + 1));
+}
+
+function flashAllButtons(arr) {
+  // arr is the array holding the number ids of the buttons to be flashed by the computer
+  var index = 0;
+  setInterval(function flashyFlash() {
+    if (index < arr.length) {
+      flashButton(arr[index]);
+      index ++;
+    } else {
+      boardActive = true;
+      clearInterval(flashyFlash);
+    }
+  }, flashInterval * 1500)
 }
 
 function flashButton(buttonID) {
-  // button will be a string number
+  // button will be a number/ string
 
-  // 7 -> #7 for queryselector
+  // 7 -> button7 for queryselector
   const buttonIDtag = "#button" + buttonID;
+  console.log(buttonIDtag);
+  console.log(typeof buttonIDtag);
 
   // select the button
   const button = document.querySelector(buttonIDtag);
-  console.log(button);
 
   // animate the button flash
-  button.style.animation = `flashButton ${flashInterval}s linear 0.1s`;
+  button.classList.add("flashing-grid-button")
 
-  // remove the animation style after button is done flashing
+  // remove the animation
   setTimeout(function() {
-    button.style.animation = "";
-  }, flashInterval*1000);
+  button.classList.remove("flashing-grid-button")
+  }, flashInterval * 1000)
+};
+
+
+function arraySame(arr1, arr2) {
+  if (arr1.length == arr2.length) {
+    for (let index = 0; index < arr1.length; index++) {
+      if (!(arr1[index] == arr2[index])) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
-
-
 
 // event listeners
 // start button clicked
-startButtonEl.addEventListener("click", function(e) {
+startButtonEl.addEventListener("click", function (e) {
   e.preventDefault();
 
   // hide the start message overlay
@@ -54,56 +81,57 @@ startButtonEl.addEventListener("click", function(e) {
 
   // randomly generate 1 new number and add it to answerArray
   addNewNumberToAnswer();
-  console.log(answerArray)
 
-  // flash the first button of the answer sequence after 1.5s
-  setTimeout(function() {
-    flashButton(answerArray[0]);
-  }, 1500);
-
-  // activate the board (allow user to click on board)
-  boardActive = true; 
-})
-
-
-
-
-
-
+  // flash the first button of the answer sequence
+  flashAllButtons(answerArray);
+});
 
 
 // user input button clicked
-gameGridEl.addEventListener("click", function(e) {
+gameGridEl.addEventListener("click", function (e) {
   e.preventDefault();
 
   // ignore click if it's in a gap
-  if ((e.target.getAttribute('class') != 'grid-button') || !boardActive) {
+  if (e.target.getAttribute("class") != "grid-button" || !boardActive) {
     return;
   }
 
   // add button number to userInputArray
-  userInputArray.push(e.target.getAttribute('id').slice(-1)); 
+  userInputArray.push(e.target.getAttribute("id").slice(-1));
   console.log(userInputArray);
+
   // increase user input index by 1
-  userInputIndex ++;
-  // flash the button
+  userInputIndex++;
+
+  // animate button press
   flashButton(userInputArray[userInputIndex]);
 
+  console.log(userInputArray, answerArray);
+
   // check if level complete (all inputs correct)
-  if (userInputArray == answerArray) {
+  if (arraySame(userInputArray, answerArray)) {
+    backgroundEl.style.backgroundColor = "green"; // visual cue
+
     // prepare for a new round
     boardActive = false; // deactivate the board
-    userInputArray = []; // reset user input array
+    userInputArray.splice(0, userInputArray.length); // reset user input array
     addNewNumberToAnswer(); // randomly gen new number and append to answer array
     levelNumberSpan.innerText = answerArray.length; // increase the level number in the UI
 
-    // flash answerArray in sequence
-    for (let index = 0; index < answerArray.length; index++) {
-      setTimeout(function() {
-        flashButton(answerArray[index]);
-      }, flashInterval/2);    
-    }
-  }
-  console.log("aaaa");
 
-})
+    // flash answerArray in sequence
+    setTimeout(function() {
+      flashAllButtons(answerArray);
+    }, 5000)
+    
+    // check if each step is correct
+  } else if (userInputArray[userInputIndex] == answerArray[userInputIndex]) {
+    return;
+
+    // step is wrong
+  } else {
+    backgroundEl.style.backgroundColor = "red"; // visual cue
+    boardActive = false;
+    return;
+  }
+});
